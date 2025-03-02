67110eb93797bf068385a01a315db87f6dbfb5b6
Xodzi <ivan.kotelnikov96@mail.ru>
2025-03-02 18:03:17 +0300

    init

--- Examples/Nodify.Calculator/Commands/OpenAppCommandViewModel.cs
+++ Examples/Nodify.Calculator/Commands/OpenAppCommandViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SkyUtils;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,9 +10,29 @@ namespace Nodify.Calculator.Commands
     class OpenAppCommandViewModel : OperationViewModel
     {
         public string Description {  get; set; }
+        public List<InstalledProgram> Programms { get; set; }
+
+        private InstalledProgramsSearcher _selectedProgram;
+        public InstalledProgramsSearcher SelectedProgram
+        {
+            get => _selectedProgram;
+            set
+            {
+                _selectedProgram = value;
+                OnPropertyChanged(nameof(SelectedProgram));
+            }
+        }
         public OpenAppCommandViewModel() {
             Output = new ConnectorViewModel();
             Input.Add(new ConnectorViewModel());
+            var result = InstalledProgramsSearcher.GetAllInstalledPrograms();
+            var filtered = InstalledProgramsSearcher.GetFilteredPrograms(result, new List<string> { ".NET", "SDK", "Toolkit", "Development", "SQL", "X32", "X64", "X86", "Tools", "Tool", "Support", "Drive" });
+            Programms = filtered.Select(p => new InstalledProgram
+            {
+                Name = p.DisplayName,
+                ExecutablePath = p.DisplayIcon
+            }).ToList();
+
         }
     }
 }
--- Examples/Nodify.Calculator/Commands/ScreenShotCommandViewModel.cs
+++ Examples/Nodify.Calculator/Commands/ScreenShotCommandViewModel.cs
@@ -14,19 +14,5 @@ namespace Nodify.Calculator.Commands
             Output = new ConnectorViewModel();
             Input.Add(new ConnectorViewModel());
         }
-        private OperationViewModel InnerOutput { get; } = new OperationViewModel
-        {
-            Title = "Output Parameters",
-            Input = { new ConnectorViewModel() },
-            Location = new Point(500, 300),
-            IsReadOnly = true
-        };
-
-        private CalculatorInputOperationViewModel InnerInput { get; } = new CalculatorInputOperationViewModel
-        {
-            Title = "Input Parameters",
-            Location = new Point(300, 300),
-            IsReadOnly = true
-        };
     }
 }
